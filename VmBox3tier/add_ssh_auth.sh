6d1cb8dfe20c39426d949a094d4fdeebae9aa429
박대희 <59219339+ParkDAY@users.noreply.github.com>
2024-12-18 14:38:17 +0900

    Delete VmBox3tier directory

--- VmBox3tier/add_ssh_auth.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env bash
-
-#ssh key 생성
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.21
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.22
-sshpass -p vagrant ssh -T -o StrictHostKeyChecking=no vagrant@192.168.1.23
